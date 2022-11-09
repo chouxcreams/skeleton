b464d3978b4faafb7abb62aaf16f8dd48446992c
Shutarou Takayama <hi.mountain110916@gmail.com>
2022-11-09 21:06:35 +0900

    schema fix

---
+++ migration/README.md
@@ -0,0 +1,13 @@
+# migration
+
+DBのDDLとmigrationスクリプトを管理するモジュール
+
+## Installation
+
+1. [Requirement](#requirement)のツールをインストールする
+2. `npm ci` を実行する
+
+## Requirement
+
+- node
+- [volta](https://github.com/volta-cli/volta)
--- migration/prisma/migrations/20221109042212_init/migration.sql
+++ migration/prisma/migrations/20221109042212_init/migration.sql
@@ -1,9 +1,12 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('TODO', 'DONE');
+
 -- CreateTable
 CREATE TABLE "Task" (
-    "taskId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "taskId" UUID NOT NULL,
+    "title" VARCHAR(50) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "TaskStatus" NOT NULL DEFAULT 'TODO',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "versionNo" BIGINT NOT NULL,
